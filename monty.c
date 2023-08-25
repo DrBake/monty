bd1cd8472be01427cdbfd366aa73fc032f00dedf
Mat-t-hew <n.m.ikenna2@gmail.com>
2023-08-24 23:37:05 -0700

    000

--- monty.c
+++ monty.c
@@ -1,24 +1,32 @@
 #include "monty.h"
 
+/**
+ * main - An interpreter for Monty ByteCodes.
+ * @argc: Number of arguments passed to the program.
+ * @argv: Array of arguments passed to the program.
+ * Return: EXIT_FAILURE or EXIT_SUCCESS if the program completes successfully.
+ */
+
 int main(int argc, char **argv)
 {
-    FILE *file;
+	FILE *file;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
-    file = fopen(argv[1], "r");
-    if (!file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+	file = fopen(argv[1], "r");
+
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
     /* TODO: Parse and execute the Monty bytecodes here */
 
-    fclose(file);
-    return (EXIT_SUCCESS);
+	fclose(file);
+	return (EXIT_SUCCESS);
 }
--- opcodes.c
+++ opcodes.c
@@ -1,41 +1,61 @@
 #include "monty.h"
 
+/**
+ * push - Pushes an integer onto a stack.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: Line number where the opcode is located.
+ * @argument: The integer (as a string) to be pushed onto the stack.
+ *
+ * Description: If the provided argument is not an integer or no argument
+ * is provided, an error message is printed to stderr and the program exits.
+ * Otherwise, the argument is converted to an integer and added to the stack.
+ */
+
 void push(stack_t **stack, unsigned int line_number, char *argument)
 {
-    stack_t *new_node;
-    int num;
+	stack_t *new_node;
+	int num;
 
-    if (argument == NULL || (num = atoi(argument)) == 0)
-    {
-        fprintf(stderr, "L%d: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	num = atoi(argument);
+	if (argument == NULL || num == 0)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    new_node = malloc(sizeof(stack_t));
-    if (!new_node)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-    new_node->n = num;
-    new_node->prev = NULL;
-    new_node->next = *stack;
-    if (*stack)
-        (*stack)->prev = new_node;
-    *stack = new_node;
+	new_node->n = num;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	if (*stack)
+		(*stack)->prev = new_node;
+	*stack = new_node;
 }
 
+/**
+ * pall - Prints all the values on the stack, starting from the top.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: Line number where the opcode is located.
+ *
+ * Description: If the stack is not empty, this function prints all its
+ * values to stdout. If the stack is empty, nothing is printed.
+ */
 void pall(stack_t **stack, unsigned int line_number)
 {
-    stack_t *current = *stack;
+	stack_t *current = *stack;
 
-    (void)line_number;  /* to avoid unused parameter warning */
+	(void)line_number;  /* to avoid unused parameter warning */
 
-    while (current)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
 }
-
