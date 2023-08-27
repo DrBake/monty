2571da61ef26df2cbb99c49f1c6acb79ab997279
Mat-t-hew <n.m.ikenna2@gmail.com>
2023-08-27 16:45:53 -0700

    2

--- opcodes_2.c
+++ opcodes_2.c
@@ -7,8 +7,20 @@
  */
 void add(stack_t **stack, unsigned int line_number)
 {
-	(void)stack;
-(void)line_number;
+	stack_t *temp;
+
+	if (!(*stack) || !((*stack)->next))
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n += (*stack)->n;
+
+	temp = *stack;
+	*stack = (*stack)->next;
+	free(temp);
+	(*stack)->prev = NULL;
 }
 
 /**
